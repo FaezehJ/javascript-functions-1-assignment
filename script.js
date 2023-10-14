3fd0aec4b3899aecd979a01368b53153002b1720
FaezehJ <faezeh.jahanshiri@gmail.com>
2023-10-14 10:48:45 -0400

    completed the exercises assignment

---
+++ script.js
@@ -0,0 +1,13 @@
+//Define a function underneath here that is named numberAdder
+//numberAdder should take 2 parameters and it should return the sum of those two values
+//After you're done, try looking around at the CSS and HTML to 
+//see if you understand what's going on :)
+
+function numberAdder(a, b) {
+    return a + b;
+}
+let sum = numberAdder(5, 10);
+console.log(sum);
+
+
+
